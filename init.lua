319388ee28e8d5040d8003dd236d9087044b940c
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-06 11:54:29 -0500

    commented out some stuff in init.lua

--- init.lua
+++ init.lua
@@ -36,19 +36,19 @@ end
 
 -- Define custom `vim.pack.add()` hook helper. See `:h vim.pack-events`.
 -- Example usage: see 'plugin/40_plugins.lua'.
-Config.on_packchanged = function(plugin_name, kinds, callback, desc)
-	local f = function(ev)
-		local name, kind = ev.data.spec.name, ev.data.kind
-		if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then
-			return
-		end
-		if not ev.data.active then
-			vim.cmd.packadd(plugin_name)
-		end
-		callback()
-	end
-	Config.new_autocmd("PackChanged", "*", f, desc)
-end
+-- Config.on_packchanged = function(plugin_name, kinds, callback, desc)
+-- 	local f = function(ev)
+-- 		local name, kind = ev.data.spec.name, ev.data.kind
+-- 		if not (name == plugin_name and vim.tbl_contains(kinds, kind)) then
+-- 			return
+-- 		end
+-- 		if not ev.data.active then
+-- 			vim.cmd.packadd(plugin_name)
+-- 		end
+-- 		callback()
+-- 	end
+-- 	Config.new_autocmd("PackChanged", "*", f, desc)
+-- end
 
 require("config.lazy")
 require("config.options")
