4d9721d343efcd4f8dd120057230380cd7334a3d
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-06 09:09:18 -0500

    mini.starter

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-vim.loader.enable()
 vim.o.shada = "'100,<50,s10,:1000,/100,@100,h" -- limit ShaDa file (for startup)
 
 _G.Config = {}
+_G.start_time = vim.loop.hrtime()
 
 require("config.lazy")
 require("config.options")
--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -1,9 +1,16 @@
 return {
 	"nvim-mini/mini.nvim",
 	version = false,
-    config = function()
-        require('mini.icons').setup()
-        require('mini.files').setup()
-        require('mini.ai').setup { n_lines = 500 }
-    end,
+	config = function()
+		require("mini.icons").setup()
+		require("mini.files").setup()
+		require("mini.ai").setup({ n_lines = 500 })
+		-- require("mini.starter").setup()
+		require("mini.starter").setup({
+			footer = function()
+				local total_time = (vim.loop.hrtime() - _G.start_time) / 1e6
+				return string.format("🚀 Startup time: %.2fms", total_time)
+			end,
+		})
+	end,
 }
