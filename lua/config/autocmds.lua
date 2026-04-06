84b1a43f0714b85799e0a8304aab709a0d180d08
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-06 01:45:36 -0500

    added comments to autocmds for clarity

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,11 @@
 -- Autocommands ===============================================================
+-- helper function for easier autocmd creation
 local gr = vim.api.nvim_create_augroup("custom-config", {})
 Config.new_autocmd = function(event, pattern, callback, desc)
 	local opts = { group = gr, pattern = pattern, callback = callback, desc = desc }
 	vim.api.nvim_create_autocmd(event, opts)
 end
+
 -- Don't auto-wrap comments and don't insert comment leader after hitting 'o'.
 -- Do on `FileType` to always override these changes from filetype plugins.
 local f = function()
@@ -11,6 +13,7 @@ local f = function()
 end
 Config.new_autocmd("FileType", nil, f, "Proper 'formatoptions'")
 
+-- highlight yanked text and cycle through numbered registers
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text, cycle numbered registers",
 	group = vim.api.nvim_create_augroup("highlight_yanked_text", { clear = true }),
