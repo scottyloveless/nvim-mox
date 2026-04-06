c876668a5fbb1add3dbdbcbf7e01c7237d45215c
scottyloveless <8176125+scottyloveless@users.noreply.github.com>
2026-04-06 01:43:09 -0500

    changed telescope bindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,12 +8,22 @@ vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<CR>", { desc = "open
 -- Telescope ====================
 local builtin = require("telescope.builtin")
 -- searching
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "files" })
-vim.keymap.set("n", "<leader>f/", builtin.live_grep, { desc = "grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[b]uffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[h]elp" })
-vim.keymap.set("n", "<leader>fv", builtin.vim_options, { desc = "[v]im options" })
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "files" })
+vim.keymap.set("n", "<leader>s/", builtin.live_grep, { desc = "grep" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[h]elp" })
+vim.keymap.set("n", "<leader>sv", builtin.vim_options, { desc = "[v]im options" })
+vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[f]ind [k]eymaps" })
+vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[s]earch [s]elect Telescope" })
 -- lsp
 vim.keymap.set("n", "<leader>dd", builtin.diagnostics, { desc = "[d]iagnostics" })
 -- ui
 vim.keymap.set("n", "<leader>uC", builtin.colorscheme, { desc = "[C]olorscheme" })
+
+vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[s]earch [s]elect Telescope" })
+vim.keymap.set({ "n", "v" }, "<leader>sw", builtin.grep_string, { desc = "[s]earch current [w]ord" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[s]earch by [g]rep" })
+vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[s]earch [d]iagnostics" })
+vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[s]earch [r]esume" })
+vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[s]earch Recent Files ("." for repeat)' })
+vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "[s]earch [c]ommands" })
+vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
